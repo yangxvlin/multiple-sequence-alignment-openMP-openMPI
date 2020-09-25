5b2dced94534c970db0c7f9a236afc8d66f1ca09
yangxvlin <1768528843@qq.com>
2020-09-25 17:14:34 +1000

    commit

--- testing15/test.cpp
+++ testing15/test.cpp
@@ -361,12 +361,12 @@ inline std::string getMinimumPenalties(std::string *genes,
                 if (task.z == NO_MORE_TASK) {
                     break;
                 }
-                // start1 = GetTimeStamp();
+                start1 = GetTimeStamp();
                 Packet p = do_task(genes[task.x], genes[task.y], task.z, pxy, pgap, genes_length[task.x], genes_length[task.y]);
                 MPI_Send(&p, 1, MPI_Packet, root, COLLECT_RESULT_TAG, comm);
-                // end1 = GetTimeStamp();
-                // cout << "rank[" << 0 << "] computes: " <<  end1 - start1 << " for task: " << task.z << " with length: " << 
-                // genes_length[task.x] << ", " << genes_length[task.y] << endl;
+                end1 = GetTimeStamp();
+                cout << "rank[" << 0 << "] computes: " <<  end1 - start1 << " for task: " << task.z << " with length: " << 
+                genes_length[task.x] << ", " << genes_length[task.y] << endl;
             } while (true);
 
             end = GetTimeStamp();
